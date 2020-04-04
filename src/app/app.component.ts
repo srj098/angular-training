eb8abc7aa7d7554a79a6b4e3e66aefb4e89b6f98
Suraj <st185170@ncr.com>
2020-04-04 20:24:22 +0530

    Day:2 event binding and two way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,21 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-demo';
-  name = 'suraj';
 
-  innerHTMLValue = 'inner html demo';
+  title: string = 'Angular-Demo';
 
-  disable = false;
 
-  count =0;
+  innerHTMLValue: string = 'bind into heading';
+  paragraph: string = "bind into paragraph";
+  imageSrc: string = "https://angular.io/assets/images/logos/angular/angular.svg";
 
-  clickMe(){
+  disable: boolean = false;
+
+  count: number = 0;
+
+
+  clickMe(): void {
     this.count++;
-    console.log(this.count);
-    if(this.count==3){
-      this.disable =true;
+    alert(this.count);
+    if (this.count == 3) {
+      this.disable = true;
     }
-    
+
   }
+
+  Color: string;
+
+  onChange(data): void {
+    this.Color = data.target.value;
+  }
+
+  inputBinding: string = "Hello"
+
 }
