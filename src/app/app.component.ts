b65673b2e2fb661ec0af51fc0ddcf3767f09d8c5
Shilpa <sk185189@ncr.com>
2020-04-05 15:12:36 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,34 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  title: string = 'Angular-Demo';
-
-
-  innerHTMLValue: string = 'bind into heading';
-  paragraph: string = "bind into paragraph";
-  imageSrc: string = "https://angular.io/assets/images/logos/angular/angular.svg";
-
-  disable: boolean = false;
-
-  count: number = 0;
-
-
-  clickMe(): void {
-    this.count++;
-    alert(this.count);
-    if (this.count == 3) {
-      this.disable = true;
-    }
-
-  }
-
-  Color: string;
-
-  onChange(data): void {
-    this.Color = data.target.value;
-  }
-
-  inputBinding: string = "Hello"
-
+  binding:string='This HTML is to test data binding in Angular';
+  htmllink='https://www.w3schools.com/html/html_basic.asp';
+  Imagelink='https://lh3.googleusercontent.com/cqK12XTJgqhFH13wqEUh224-ItIyxqkvFSU8k9-h6u-A4y6yHs_saID8KFB1OTPYtrk=s180-rw';
+  alttext='ncr.com';
+  
 }
