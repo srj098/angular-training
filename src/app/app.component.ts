d1d210989733b2557ba9402b402e75e41b96647d
Shilpa <sk185189@ncr.com>
2020-04-05 20:23:44 +0530

    Added html and component file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  binding:string='This HTML is to test data binding in Angular';
-  htmllink='https://www.w3schools.com/html/html_basic.asp';
-  Imagelink='https://lh3.googleusercontent.com/cqK12XTJgqhFH13wqEUh224-ItIyxqkvFSU8k9-h6u-A4y6yHs_saID8KFB1OTPYtrk=s180-rw';
+  binding:string='NCR Employee Information Form';
+  htmllink='https://www.ncr.com/';
+  Logo='https://lh3.googleusercontent.com/cqK12XTJgqhFH13wqEUh224-ItIyxqkvFSU8k9-h6u-A4y6yHs_saID8KFB1OTPYtrk=s180-rw';
   alttext='ncr.com';
-  
+  size1='font-size:150%';
+  onSubmit(){
+    alert('Submit');
+  }
+  fname:string='Shilpa';
+  lname:string='Kulkarni';
+
 }
