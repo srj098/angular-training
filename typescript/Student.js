7346503643f4b082c9ee6c76201109ad97b499c3
Suraj <st185170@ncr.com>
2020-04-24 00:37:02 +0530

    typescript

---
+++ typescript/Student.js
@@ -0,0 +1,8 @@
+var Student = /** @class */ (function () {
+    function Student() {
+    }
+    Student.prototype.getStudentName = function () {
+        return 'Suraj';
+    };
+    return Student;
+}());
